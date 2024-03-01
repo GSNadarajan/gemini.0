797c02b055c1707491f5fbc1934912e77deb1a50
GSNadarajan <nadarajangs31@gmail.com>
2024-03-01 19:37:01 +0530

    customized UI

--- src/App.js
+++ src/App.js
@@ -57,28 +57,63 @@ const App = () => {
 
   const clear = () => {
     setValue("");
-    setError("")
+    setError("");
     setChatHistory([]);
-  }
+  };
 
   return (
     <div className="app">
-      <p>
-        What do want to know?
-        <button className="surprise" onClick={surprise} disabled={!chatHistory}>
-          Surprise me
-        </button>
-      </p>
+      <h2 style={{ fontSize: "20px", text: "center" }}>
+        Gemini-pro model{" "}
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="26"
+            style={{ marginTop: "1px"}}
+            fill="currentColor"
+            class="bi bi-lightning-charge-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09z" />
+          </svg>
+        </span>
+      </h2>
+      <p></p>
       <div className="input-container">
         <input
           value={value}
-          placeholder="When is Christmas...?"
+          placeholder=" What do want to know ?"
           onChange={(e) => setValue(e.target.value)}
         />
-        {!error && <button onClick={getResponse}>Ask me</button>}
-        {error && <button onClick={clear}>Clear</button>}
+        {!error && (
+          <button
+            onClick={getResponse}
+            style={{ background: "#808080", color: "white", fontSize: "18px" }}
+          >
+            Ask me
+          </button>
+        )}
+        {/* {error && <button onClick={clear}>Clear</button>} */}
       </div>
       {error && <p>{error}</p>}
+      <div style={{ marginTop: "30px" }}>
+        <button
+          className="surprise"
+          style={{ fontSize: "15px", padding: "9px" }}
+          onClick={surprise}
+          disabled={!chatHistory}
+        >
+          Surprise me
+        </button>
+        <button
+          className="surprise"
+          style={{ fontSize: "15px", padding: "9px" }}
+          onClick={clear}
+        >
+          Clear
+        </button>
+      </div>
       <div className="search-result">
         {chatHistory.map((chatItem, _index) => (
           <div key={_index}>
